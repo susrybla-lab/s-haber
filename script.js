de58b248ec99571bae8d63b63d2732ecf5967ac4
susrybla-lab <susrybla@gmail.com>
2026-03-15 16:46:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ setInterval(() => {
     changeSlide(1);
 }, 5000);
 
+const themeBtn = document.getElementById('theme-toggle');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    
+    // Tercihi hafızaya al (Sayfa yenilense de gitmez)
+    const isDark = document.body.classList.contains('dark-theme');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+});
+
+// Sayfa açıldığında hafızadaki temayı yükle
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+}
+
 // Arama Fonksiyonu
 function searchNews() {
     const input = document.getElementById('searchInput').value.toLowerCase();
